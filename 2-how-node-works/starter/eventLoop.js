507baaca53a1d6ebf4b72b16ed3b847f825f89a4
John Macaday <johnelsman.macaday@gmail.com>
2022-12-17 21:53:50 +0800

    Section 4-36

--- 2-how-node-works/starter/eventLoop.js
+++ 2-how-node-works/starter/eventLoop.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const crypto = require('crypto');
 
+const start = Date.now();
+// process.env.UV_THREADPOOL_SIZE = 1;
+
 setTimeout(() => console.log('Timer 1 finished'), 0);
 setImmediate(() => console.log('Immediate 1 finished'));
 
@@ -15,7 +18,16 @@ fs.readFile('test-file.txt', () => {
     process.nextTick(() => console.log('process.nextTick()'));
 
     crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
-        console.log('password encrypted')
+        console.log(Date.now() - start, 'password encrypted')
+    })
+    crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
+        console.log(Date.now() - start, 'password encrypted')
+    })
+    crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
+        console.log(Date.now() - start, 'password encrypted')
+    })
+    crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
+        console.log(Date.now() - start, 'password encrypted')
     })
 });
 
