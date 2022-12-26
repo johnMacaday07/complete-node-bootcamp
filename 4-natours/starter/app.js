ed6a186a61ef242c754c296e9167de611eee0a9c
johnMacaday07 <53150870+johnMacaday07@users.noreply.github.com>
2022-12-26 14:58:42 +0800

    Section 9-118

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const errorController = require('./controllers/errorController');
 
 const app = express();
 
@@ -14,10 +16,10 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware 👋');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -27,5 +29,22 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// 404, unhandled routes
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl}`
+  // });
+  // const err = new Error(`Can't find ${req.originalUrl}`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppError(`Can't find ${req.originalUrl}`, 404));
+  // if next(hasParam), will skip to error handling middleware
+});
+
+// error handling middleware, auto detect when cb function has 4 params
+app.use(errorController);
 
 module.exports = app;
